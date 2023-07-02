fe3bf5a68c330c59f32ec4b851b47c2f80f93bb5
yashdkadam <kadamdyash55@gmail.com>
2023-07-02 15:57:52 +0530

    intial commit

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const linkedIn = require('linkedin-jobs-api');
 const app = express()
 app.all('/', (req, res) => {
+    const queryOptions = {
+        keyword: 'software engineer',
+        location: 'India',
+        dateSincePosted: 'past Week',
+        jobType: 'full time',
+        remoteFilter: 'remote',
+        salary: '100000',
+        experienceLevel: 'entry level',
+        limit: '10'
+    };
+
+    linkedIn.query(queryOptions).then(response => {
+        console.log(response); // An array of Job objects
+    });
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('seding response')
 })
 app.listen(process.env.PORT || 3000)
