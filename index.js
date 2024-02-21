43943de6a2ac93a55252266af8247239a023f4ef
Yash Kadam <81897342+yashdkadam@users.noreply.github.com>
2024-02-21 16:59:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-const express = require('express')
-const linkedIn = require('linkedin-jobs-api');
-const app = express()
-app.all('/', (req, res) => {
-    const queryOptions = {
-        keyword: 'software engineer',
-        location: 'India',
-        dateSincePosted: 'past Week',
-        jobType: 'full time',
-        remoteFilter: 'remote',
-        salary: '100000',
-        experienceLevel: 'entry level',
-        limit: '10'
-    };
+const express = require('express');
+const { Snowfl, Sort } = require('snowfl-api');
 
-    linkedIn.query(queryOptions).then(response => {
-        console.log(response); // An array of Job objects
-    });
-    console.log("Just got a request!")
-    res.send('seding response')
-})
-app.listen(process.env.PORT || 3000)
+const app = express();
+const snowfl = new Snowfl();
+
+// Define your route with query parameters
+app.get('/fetchData', async (req, res) => {
+  const { query, nsfw } = req.query;
+
+  try {
+    let data = await fetchData(query, includeNsfw);
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Function to fetch data
+async function fetchData(query, includeNsfw) {
+  try {
+    let options = {
+      sort: Sort.MAX_SEED,
+      includeNsfw: includeNsfw === 'true' // Convert string to boolean
+    };
+    let res = await snowfl.parse(query || 'JoJo', options);
+    return res;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
